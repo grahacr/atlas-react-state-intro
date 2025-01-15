1a1c0529c618d70f63e0ec2fc12e0b69b45ae190
grahacr <courtney.graham@atlasschool.com>
2025-01-14 21:52:51 -0600

    loading data from json api file using fetch and rendering it dynamically using useEffect

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function SchoolCatalog() {
+  const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/courses.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCourseData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
@@ -15,36 +30,18 @@ export default function SchoolCatalog() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>PP1000</td>
-            <td>Beginning Procedural Programming</td>
-            <td>2</td>
-            <td>30</td>
+          {courseData.map((course, index) => (
+          <tr key={index}>
+            <td>{course.trimester}</td>
+            <td>{course.courseNumber}</td>
+            <td>{course.courseName}</td>
+            <td>{course.semesterCredits}</td>
+            <td>{course.totalClockHours}</td>
             <td>
               <button>Enroll</button>
             </td>
           </tr>
-          <tr>
-            <td>1</td>
-            <td>PP1100</td>
-            <td>Basic Procedural Programming</td>
-            <td>4</td>
-            <td>50</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>OS1000</td>
-            <td>Fundamentals of Open Source Operating Systems</td>
-            <td>2.5</td>
-            <td>37.5</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
+          ))}
         </tbody>
       </table>
       <div className="pagination">
